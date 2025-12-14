883d7c2b850c19e50018e36e87151c56199f5b2b
madvin <madvin@abv.bg>
2025-12-14 12:19:20 +0200

    added some effects to the text

--- react/src/components/Hero.jsx
+++ react/src/components/Hero.jsx
@@ -1,7 +1,9 @@
 export default function Hero() {
   return (
-    <>
-        <div className=''> Hero</div>
-    </>
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center'>
+        <p className=''>IT'S TIME TO</p>
+        <h1>stand <span className='text-blue-600'>UP</span> and <span className='text-blue-600'>train hard</span></h1>
+        <p className='text-blue-600'>YOUR WORKOUT</p>
+    </div>
   );
 }
