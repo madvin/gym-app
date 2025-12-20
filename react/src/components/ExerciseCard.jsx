94eae86ec96d679aae963bbfd9d91f726536f3e5
madvin <madvin@abv.bg>
2025-12-20 20:47:40 +0200

    add ExerciseCard component and Button component; update Generator layout and functions.js imports

---
+++ react/src/components/ExerciseCard.jsx
@@ -0,0 +1,5 @@
+export default function ExerciseCard() {
+  return (
+    <>content</>
+  );
+}
--- react/src/components/Workout.jsx
+++ react/src/components/Workout.jsx
@@ -1,8 +1,17 @@
+import ExerciseCard from "./ExerciseCard";
+import SectionWrapper from "./SectionWrapper";
+
 export default function Workout(props) {
   const { workout } = props;
   return (
-    <>
-        <div className=''> Workout</div>
-    </>
+    <SectionWrapper header={'welcome to'} title={['The', 'DANGER', 'zone']}>
+      <div className='flex flex-col gap-4'>
+       {workout.map((exercise, i) => {
+          return (
+            <ExerciseCard />
+          )
+       })}    
+      </div>
+    </SectionWrapper>
   );
 }
