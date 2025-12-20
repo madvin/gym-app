62e0ade12e40667c1656fbdaaac8f55fda4da478
madvin <madvin@abv.bg>
2025-12-20 20:02:10 +0200

    add button effects

--- react/src/components/Hero.jsx
+++ react/src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import Button from './Button';
+
 export default function Hero() {
   return (
     <div className='min-h-screen flex flex-col gap-12 items-center justify-center text-center max-w-[1200px] w-full mx-auto p-4'>
@@ -6,9 +8,7 @@ export default function Hero() {
         <h1 className='uppercase font-semibold text-5xl sm:text-6xl md:text-7xl'>stand <span className='text-blue-500'>UP</span> and <span className='text-blue-500'>train hard</span></h1>
          </div>
         <p className='text-sm md:text-base font-light'>YOUR WORKOUT</p>
-        <button className='px-8 py-4 rounded-medium'>
-            <p className='text-blue-600'>Accept</p>
-        </button>
+        <Button text={'Accept & Begin'}></Button>
     </div>
   );
 }
